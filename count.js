a2fa036191cce5ce74f0b9a76498a129c353a7cf
devangriffin <89404422+devangriffin@users.noreply.github.com>
2023-10-04 16:16:37 +0000

    Count Function Start

---
+++ count.js
@@ -0,0 +1,21 @@
+function traverseTree(json)
+{
+    json.targets[index].blocks;
+    let blocks
+    if (opcode === NULL) { return 0; }
+    if (blocks === NULL) { return 0; }
+
+    traverseTree(node.child);
+}
+
+function CountBlockPerType(opcode, inputBlockType, jsoninput)
+{
+    const code = opcode.split("_");
+    code[0];
+
+    if (code[0] = inputBlockType) {
+        
+    } 
+}
+
+object.targets.blocks

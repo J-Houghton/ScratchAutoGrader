7c4a9b3707d6a1b202907b18c0e9f8be04e92e77
Jake Houghton <96552435+J-Houghton@users.noreply.github.com>
2023-10-04 15:19:06 +0000

    fixed parser message

--- parser.js
+++ parser.js
@@ -1,24 +1,20 @@
 const fs = require('fs').promises;
 
-//const projectJSON = require('./unzippedSb3/project.json');
-
 async function parse(filePath) {
     console.log("parsing...");
     console.log(filePath);
 
-    const jsonData = await fs.readFile(filePath, 'utf-8');
-    const largeJsonObject = JSON.parse(jsonData);
-    const jsonString = JSON.stringify(largeJsonObject, null, 2); // 'null' and '2' are used for formatting
+    try {
+        const jsonData = await fs.readFile(filePath, 'utf-8');
+        const largeJsonObject = JSON.parse(jsonData);
+        const jsonString = JSON.stringify(largeJsonObject, null, 2); // 'null' and '2' are used for formatting
 
-// Write the formatted JSON string to a file
-    fs.writeFile('output.json', jsonString, (err) => {
-    if (err) {
-        console.error('Error writing to file', err);
-    } else {
+        // Using promises to write to the file
+        await fs.writeFile('output.json', jsonString);
         console.log('Successfully wrote to file');
+    } catch (err) {
+        console.error('Error writing to file', err);
     }
-    });
-
 }
 
 module.exports = parse;

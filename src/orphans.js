86f6bd0e5602112534b0de049a995986389deb9e
janisly <89416192+janisly@users.noreply.github.com>
2023-10-25 16:24:09 +0000

    function progress

--- src/orphans.js
+++ src/orphans.js
@@ -1,91 +1,94 @@
 #!/usr/bin/env node
 
 import fs from 'fs/promises';
-
-// Grab the file path from command-line arguments
-const filePath = process.argv[2]; 
-
-if (!filePath) {
-    console.error("Please provide a file path as an argument.");
-    process.exit(1);
-}
-//const data = require('./' + filePath);
-//var aggg = './' + filePath;
-//import data from aggg;
-//const data = astRootNode;
 import * as data from './output_ast.json' assert {type : 'json'};
-//import data from './output_ast.json' assert {type = JSON};
-console.log(data);
+export function orphanSort(filePath)
+{
+    // Grab the file path from command-line arguments
+    //const filePath = process.argv[2]; 
 
-const orphans = [];
-const nonOrphans = [];
-const unDecided = [];
-
-const targets = data.default.data.targets;
-console.log(targets[0].blocks);
-
-for (let i = 0; i < targets.length; i++) {
-    var blocks = targets[i].blocks;
-    var unDecidedSub = [];
-    var blockKeys = Object.keys(blocks);
-
-    for (let i = 0; i < blockKeys.length; i++) {
-        var name = blockKeys[i];
-        var opcode = blocks[name]['opcode'];
-        var topLevel = blocks[name]['topLevel'];
-        var parent = blocks[name]['parent'];
-        //if(toplevel == true)
-        if(parent == null){
-            if(opcode == 'event_whenflagclicked' || opcode == 'event_whenkeypressed' || opcode == 'event_whenstageclicked' 
-            || opcode == 'event_whenbackdropswitchesto' || opcode == 'event_whengreaterthan' || opcode == 'event_whenbroadcastreceived'){
-                //console.log(name + " is nonorphan!");
-                nonOrphans.push(blocks[name]);
-            }
-            else{
-                //console.log(name + " is orphaned!")
-                orphans.push(blocks[name]);
-            }
-        }
-        else{ 
-            if(!unDecided.includes(name)){
-                unDecidedSub.push(blocks[name]); 
-            }
-        }
+    if (!filePath) {
+        console.error("Please provide a file path as an argument.");
+        process.exit(1);
     }
-    unDecided.push(unDecidedSub);
-}
-//console.info(unDecided);
+    //const data = require('./' + filePath);
+    //var aggg = './' + filePath;
+    //import data from aggg;
+    //const data = astRootNode;
+    //import data from './output_ast.json' assert {type = JSON};
+    console.log(data);
 
-for (let i = 0; i < targets.length; i++) {
-    var blocks = targets[i].blocks;
-    var blockKeys = Object.keys(blocks);
-    var unDecidedSub = unDecided[i];
-    //console.log(unDecidedSub.length);
-    for(let i = 0; i < unDecidedSub.length; i++){
-        var blockName = unDecidedSub[i];
-        //console.log(blockName);
-        //console.log(blocks[blockName]);
-        if(blocks[blockName].parent == null){
-            console.log("Null parent error");
+    const orphans = [];
+    const nonOrphans = [];
+    const unDecided = [];
+
+    const targets = data.default.data.targets;
+    console.log(targets[0].blocks);
+
+    for (let i = 0; i < targets.length; i++) {
+        var blocks = targets[i].blocks;
+        var unDecidedSub = [];
+        var blockKeys = Object.keys(blocks);
+
+        for (let i = 0; i < blockKeys.length; i++) {
+            var name = blockKeys[i];
+            var opcode = blocks[name]['opcode'];
+            var topLevel = blocks[name]['topLevel'];
+            var parent = blocks[name]['parent'];
+            //if(toplevel == true)
+            if(parent == null){
+                if(opcode == 'event_whenflagclicked' || opcode == 'event_whenkeypressed' || opcode == 'event_whenstageclicked' 
+                || opcode == 'event_whenbackdropswitchesto' || opcode == 'event_whengreaterthan' || opcode == 'event_whenbroadcastreceived'){
+                    //console.log(name + " is nonorphan!");
+                    nonOrphans.push(blocks[name]);
+                }
+                else{
+                    //console.log(name + " is orphaned!")
+                    orphans.push(blocks[name]);
+                }
+            }
+            else{ 
+                if(!unDecided.includes(name)){
+                    unDecidedSub.push(blocks[name]); 
+                }
+            }
         }
-        else if(orphans.includes(blocks[blockName].parent)){
-            //console.log(blocks[blockName] + " orphan descendant");
-            orphans.push(blocks[blockName]);
+        unDecided.push(unDecidedSub);
+    }
+    //console.info(unDecided);
+
+    for (let i = 0; i < targets.length; i++) {
+        var blocks = targets[i].blocks;
+        var blockKeys = Object.keys(blocks);
+        var unDecidedSub = unDecided[i];
+        //console.log(unDecidedSub.length);
+        for(let i = 0; i < unDecidedSub.length; i++){
+            var blockName = unDecidedSub[i];
+            //console.log(blockName);
+            //console.log(blocks[blockName]);
+            if(blocks[blockName].parent == null){
+                console.log("Null parent error");
+            }
+            else if(orphans.includes(blocks[blockName].parent)){
+                //console.log(blocks[blockName] + " orphan descendant");
+                orphans.push(blocks[blockName]);
+            }
+            else if(nonOrphans.includes(blocks[blockName].parent)){
+                //console.log(blocks[blockName] + " nonorphan descendant");
+                nonOrphans.push(blocks[blockName]);
+            }
+            else{console.log("HUH? HUH? HUH?")}
         }
-        else if(nonOrphans.includes(blocks[blockName].parent)){
-            //console.log(blocks[blockName] + " nonorphan descendant");
-            nonOrphans.push(blocks[blockName]);
-        }
-        else{console.log("HUH? HUH? HUH?")}
+
+
     }
 
 
+
+    console.log("nonorphans: ");
+    console.info(JSON.stringify(nonOrphans));
+    console.log("orphans: ");
+    console.info(JSON.stringify(orphans));
+    var result = [orphans, nonOrphans];
+    return result;
 }
-
-
-
-console.log("nonorphans: ");
-console.info(nonOrphans);
-console.log("orphans: ");
-console.info(orphans);
-
--- src/test/test.js
+++ src/test/test.js
@@ -1,22 +1,13 @@
-/*import assert from 'assert';
+import assert from 'assert';
 import should from 'chai';
 import fs from 'fs/promises';
-//import fs from 'fs/promises';
-//var assert = require('assert');
-//var should = require('chai');
-//const fs = require('fs');
 
-import orphanSort from '../src/orphans.js';
-import unzipSb3 from '../src/sb3Unzipp.js';
-import parse from '../src/parser.js';
-import count from '../src/count.js';
-//const orphanSort = require('../orphans.js');
-//const unzipSb3 = require('../sb3Unzipp.js');
-//const parse = require('../parser.js');
-//const count = require('../count.js');
+import { orphanSort } from '../orphans.js';
+import { unzipSb3 } from '../sb3Unzipp.js';
+import { Parser } from '../parser.js';
+//import count from '../count.js';
 
-//import fs from 'fs/promises';
-*/
+
 describe('Array Baseline', function () {
   describe('#baseline()', function () {
     it('Should return -1 when the value is not present', function () {
@@ -25,91 +16,51 @@ describe('Array Baseline', function () {
   });
 });
 
+// Compares orphans output to given output from using 3-hello1.sb3
 describe('Orphan result', function () {
   describe('#orphansFile()', function () {
-    var comparison = [[
-      {
-        opcode: 'looks_say',
-        next: null,
-        parent: null,
-        inputs: { MESSAGE: [Array] },
-        fields: {}
-      }
-    ], [
-      {
-        opcode: 'event_whenflagclicked',
-        next: null,
-        parent: null,
-        inputs: {},
-        fields: {}
-      },
-      {
-        opcode: 'event_whenkeypressed',
-        next: null,
-        parent: null,
-        inputs: {},
-        fields: { KEY_OPTION: [Array] }
-      },
-      {
-        opcode: 'event_whenstageclicked',
-        next: null,
-        parent: null,
-        inputs: {},
-        fields: {}
-      },
-      {
-        opcode: 'event_whenbackdropswitchesto',
-        next: null,
-        parent: null,
-        inputs: {},
-        fields: { BACKDROP: [Array] }
-      },
-      {
-        opcode: 'event_whengreaterthan',
-        next: null,
-        parent: null,
-        inputs: { VALUE: [Array] },
-        fields: { WHENGREATERTHANMENU: [Array] }
-      },
-      {
-        opcode: 'event_whenbroadcastreceived',
-        next: null,
-        parent: null,
-        inputs: {},
-        fields: { BROADCAST_OPTION: [Array] }
-      }
-    ]]
+    var comparison = [
+      [{"opcode":"looks_say","next":null,"parent":null,"inputs":{"MESSAGE":[1,[10,"Hello!"]]},"fields":{}}],
+      [{"opcode":"event_whenflagclicked","next":null,"parent":null,"inputs":{},"fields":{}},{"opcode":"event_whenkeypressed",
+      "next":null,"parent":null,"inputs":{},"fields":{"KEY_OPTION":["space",null]}},{"opcode":"event_whenstageclicked","next":null,
+      "parent":null,"inputs":{},"fields":{}},{"opcode":"event_whenbackdropswitchesto","next":null,"parent":null,"inputs":{},
+      "fields":{"BACKDROP":["backdrop1",null]}},{"opcode":"event_whengreaterthan","next":null,"parent":null,"inputs":{"VALUE":[1,[4,"10"]]},
+      "fields":{"WHENGREATERTHANMENU":["LOUDNESS",null]}},{"opcode":"event_whenbroadcastreceived","next":null,"parent":null,
+      "inputs":{},"fields":{"BROADCAST_OPTION":["message1","~74LMZ1TPu$[YfyU2p^M"]}}]
+    ]
     it('Should create an output file with expected contents', function () {
-      var result = orphanSort('/test/testFiles/output_ast.json');
+      var result = orphanSort('/src/test/testFiles/output_ast.json');
+      console.info(result);
       assert.deepEqual(result, comparison);
     });
   });
 });
 
-describe('Unzipsb3 result', function () {
-  describe('#unzippedsb3File()', function () {
-    it('Should find the project file when unzipped', function () {
-      unzipSb3('sb3Files/3-hello1.sb3');
-      var filepath = './unzippedSb3/project.json'
-      let fileExists = fs.existsSync(filepath); 
-      assert.equal(fileExists, true);
+describe('Unzipsb3 result', async function () {
+  describe('#unzippedsb3File()', async function () {
+    it('Should find the project file when unzipped', async function () {
+      var filepath = await unzipSb3('sb3Files/3-hello1.sb3');
+      
+      let file = await fs.open(filepath); 
+      console.log(file);
+      assert(file);
+      
     });
   });
 });
 
-describe('Parser result', function () {
-  describe('#unzippedsb3File()', function () {
-    it('Should find the project file when parsed', function () {
-      parse('./unzippedSb3/project.json').catch(error => {
-        console.error("An error occurred for parse:", error);
-      });
-      var filepath = 'output.json'
-      let fileExists = fs.existsSync(filepath); 
-      assert.equal(fileExists, true);
+describe('Parser result', async function () {
+  describe('#unzippedsb3File()', async function () {
+    it('Should find the project file when parsed', async function () {
+      var filepath = await unzipSb3('sb3Files/3-hello1.sb3');
+      const parser = new Parser(); 
+      const astRootNode = await parser.parse(filepath);
+      console.log(astRootNode);
+      assert(astRootNode);
     });
   });
 });
-
+/*
 describe('Count result', function () {
   describe('#unzippedsb3File()', function () {
     it('Should return correct block type count', function () {
@@ -121,7 +72,7 @@ describe('Count result', function () {
     });
   });
 });
-
+/*
 describe('Count result', function () {
   describe('#unzippedsb3File()', function () {
     it('Should return correct type of block count', function () {
@@ -133,3 +84,4 @@ describe('Count result', function () {
     });
   });
 });
+*/
